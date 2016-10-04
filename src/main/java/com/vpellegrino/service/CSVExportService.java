dd8c7a067d71532c7b7ef94af0f3febfa0bd7f5d
vpellegrino <pellegrino.valentino@gmail.com>
2016-10-04 09:11:33 +0200

    Update CSVExportService.java

--- src/main/java/com/vpellegrino/service/CSVExportService.java
+++ src/main/java/com/vpellegrino/service/CSVExportService.java
@@ -14,7 +14,7 @@ import com.vpellegrino.model.Entity;
 /**
  * This class provides export in CSV format files
  * 
- * @author vpellegr
+ * @author vpellegrino
  *
  */
 public class CSVExportService {
